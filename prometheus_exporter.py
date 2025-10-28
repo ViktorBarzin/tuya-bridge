ec1f6fda6da5747b93384142f53636fc07f75ca2
Viktor Barzin <vbarzin@gmail.com>
2025-10-28 22:15:13 +0000

    use original tuya labels instead of renaming

--- prometheus_exporter.py
+++ prometheus_exporter.py
@@ -10,34 +10,32 @@ from prometheus_client import (
 registry = CollectorRegistry()
 
 metrics = {
-    "fault": Gauge("tuya_fault_code", "Device fault code", registry=registry),
-    "power_fault": Gauge("tuya_power_fault", "Power fault flag", registry=registry),
-    "load_power": Gauge("tuya_load_power_watts", "Load power (W)", registry=registry),
-    "load_current": Gauge(
-        "tuya_load_current_amps", "Load current (A)", registry=registry
-    ),
+    "fault": Gauge("fault", "Device fault code", registry=registry),
+    "power_fault": Gauge("power_fault", "Power fault flag", registry=registry),
+    "load_power": Gauge("load_power_watts", "Load power (W)", registry=registry),
+    "load_current": Gauge("load_current_amps", "Load current (A)", registry=registry),
     "overpower_value": Gauge(
-        "tuya_overpower_value", "Overpower threshold", registry=registry
+        "overpower_value", "Overpower threshold", registry=registry
     ),
     "lowpower_switch": Gauge(
-        "tuya_lowpower_switch", "Low power threshold", registry=registry
+        "lowpower_switch", "Low power threshold", registry=registry
     ),
     "lowpower_reset": Gauge(
-        "tuya_lowpower_reset", "Low power reset threshold", registry=registry
+        "lowpower_reset", "Low power reset threshold", registry=registry
     ),
     "totalele_add": Gauge(
-        "tuya_total_energy_wh", "Total accumulated energy (Wh)", registry=registry
+        "totalele_add", "Total accumulated energy (Wh) from inverter", registry=registry
     ),
     "dwele_add": Gauge(
-        "tuya_partial_energy_wh", "Partial energy (Wh)", registry=registry
+        "dwele_add", "Total accumulated energy (Wh) from grid", registry=registry
     ),
-    "voltage_l1": Gauge("tuya_voltage_l1_volts", "L1 voltage (V)", registry=registry),
-    "voltage_l2": Gauge("tuya_voltage_l2_volts", "L2 voltage (V)", registry=registry),
+    "voltage_l1": Gauge("voltage_l1_volts", "L1 voltage (V)", registry=registry),
+    "voltage_l2": Gauge("voltage_l2_volts", "L2 voltage (V)", registry=registry),
     "voltage_batt": Gauge(
-        "tuya_voltage_battery_volts", "Battery voltage (V)", registry=registry
+        "voltage_battery_volts", "Battery voltage (V)", registry=registry
     ),
     "power_mode": Gauge(
-        "tuya_power_mode", "Power source (0=grid,1=inverter)", registry=registry
+        "power_mode", "Power source (0=grid,1=inverter)", registry=registry
     ),
 }
 
