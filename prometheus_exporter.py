7014749e7b3ccbd4ee528aed809ef384ec4f2f73
Viktor Barzin <vbarzin@gmail.com>
2025-11-09 20:54:41 +0000

    cache tuya response for 30 sec

--- prometheus_exporter.py
+++ prometheus_exporter.py
@@ -1,5 +1,6 @@
 from abc import abstractmethod
 import time
+import cachetools
 from typing import Any, override
 from tenacity import retry, stop_after_attempt, wait_exponential
 import tinytuya
@@ -11,6 +12,8 @@ from prometheus_client import (
 
 from metrics_definition import AutomaticTransferSwitch, Fuse, MetricsDefinition
 
+cache = cachetools.TTLCache(maxsize=100, ttl=30)
+
 device_id_to_metrics: dict[str, type[MetricsDefinition]] = {
     "bfe98afa941d5a1e2def8s": AutomaticTransferSwitch,
     "bf62301ef04e38d881ugcu": Fuse,  # main
@@ -18,6 +21,7 @@ device_id_to_metrics: dict[str, type[MetricsDefinition]] = {
 }
 
 
+@cachetools.cached(cache)
 @retry(stop=stop_after_attempt(10), wait=wait_exponential(multiplier=1, min=3, max=300))
 def collect_metrics(cloud: tinytuya.Cloud, device_id: str) -> CollectorRegistry:
     registry = CollectorRegistry()
